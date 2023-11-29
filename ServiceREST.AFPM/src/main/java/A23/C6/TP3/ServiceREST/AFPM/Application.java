163dc6fb4dbaf22170e17f0ff442a83109c68749
PaulinMb <125313649+PaulinMb@users.noreply.github.com>
2023-11-29 15:26:37 -0500

    ajout serviceRest

---
+++ ServiceREST.AFPM/src/main/java/A23/C6/TP3/ServiceREST/AFPM/Application.java
@@ -0,0 +1,13 @@
+package A23.C6.TP3.ServiceREST.AFPM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
