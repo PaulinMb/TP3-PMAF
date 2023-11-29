c0e929d480a9806929b2cc5957a16bb4a19d571d
2166316 <2166316@cegeplimoilou.ca>
2023-11-29 16:04:28 -0500

    Ajout

--- ServiceREST.PMAF/src/main/java/A23/C6/TP3/ServiceREST/PMAF/db/BestRoute.java
+++ ServiceREST.PMAF/src/main/java/A23/C6/TP3/ServiceREST/PMAF/db/BestRoute.java
@@ -1,2 +1,4 @@
-package A23.C6.TP3.ServiceREST.PMAF.db;public class BestRoute {
+package A23.C6.TP3.ServiceREST.PMAF.db;
+
+public class BestRoute {
 }
--- ServiceREST.PMAF/src/main/java/A23/C6/TP3/ServiceREST/PMAF/db/BestRouteService.java
+++ ServiceREST.PMAF/src/main/java/A23/C6/TP3/ServiceREST/PMAF/db/BestRouteService.java
@@ -1,2 +1,39 @@
-package A23.C6.TP3.ServiceREST.PMAF.db;public class BestRouteService {
+package A23.C6.TP3.ServiceREST.PMAF.db;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "fastestRoute")
+public class BestRouteService {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "bestRoute")
+    private String columnName;
+
+    public BestRouteService() {
+    }
+
+    public BestRouteService(Long id, String columnName) {
+        this.id = id;
+        this.columnName = columnName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getColumnName() {
+        return columnName;
+    }
+
+    public void setColumnName(String columnName) {
+        this.columnName = columnName;
+    }
 }
